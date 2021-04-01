466195de2b7f4cacad473e678d5e48f3c7fbba3e
Ryan Jones <ryanajonesdev@gmail.com>
2021-03-31 23:48:36 -0700

    started set up api.js with database schema and fetch to proxy

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,59 @@
+const { json } = require('body-parser');
+const fetch = require('node-fetch');
+const mongoose = require('mongoose');
+
+mongoose.set('useFindAndModify', false);
+
+mongoose.connect(
+  process.env.DB,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, db) => {
+    if (err) return console.log(err);
+    console.log('Successful database connection.');
+  }
+);
+
+// Mongoose schema
+const { Schema } = mongoose;
+
+const stockSchema = new Schema({
+  symbol: String,
+  price: Number,
+  like: Boolean,
+});
+
+// Mongoose model
+const Stocks = mongoose.model('stocks', stockSchema);
+
 module.exports = function (app) {
-  app.route('/api/stock-prices').get(function (req, res) {});
+  app.route('/api/stock-prices').get(function (req, res) {
+    const { stock, like } = req.query;
+    console.log(stock);
+
+    // Fetch stock price from proxy
+    async function getStockPrice(stockSymbol) {
+      try {
+        const response = await fetch(
+          `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stockSymbol}/quote`
+        );
+        const data = await response.json();
+        return res.json({
+          stockData: { stock: data.symbol, price: data.latestPrice },
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    // Check to see if stock is present
+    if (stock) {
+      // Handle if there are two stocks
+      if (!Array.isArray(stock)) {
+        getStockPrice(stock);
+      }
+    }
+  });
 };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const apiRoutes = require('./routes/api.js');
 const fccTestingRoutes = require('./routes/fcctesting.js');
@@ -16,6 +17,21 @@ app.use(cors({ origin: '*' })); // For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Helmet secrutiy middleware
+/* app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: [
+        "'self'",
+        "'unsafe-inline'",
+        'https://code.jquery.com/jquery-2.2.1.min.js',
+      ],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+    },
+  })
+);
+ */
 // Index page (static HTML)
 app.route('/').get(function (req, res) {
   res.sendFile(`${process.cwd()}/views/index.html`);
